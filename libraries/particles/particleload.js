b94fc41bf28e4f96e2703100a6c0e2ebe28db0b7
Alex Mustafich-Nutley <alexmnutley@gmail.com>
2019-02-19 17:10:10 -0500

    integrated particle background, changed file structure for organization

---
+++ libraries/particles/particleload.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'libraries/particles/particleconfig.json', function() {
+  console.log('callback - particles.js config loaded');
+});
