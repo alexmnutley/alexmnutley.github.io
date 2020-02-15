69b5f4840d573521a29326cfb2c4333bb4ac2bc1
Alex Mustafich-Nutley <alexmnutley@gmail.com>
2020-02-15 14:04:49 -0500

    please just function

--- libraries/particles/particleload.js
+++ libraries/particles/particleload.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'particleconfig.json', function() {
+particlesJS.load('particles-js', 'libraries/particles/particleconfig.json', function() {
     console.log('particles.js loaded - callback');
   });
